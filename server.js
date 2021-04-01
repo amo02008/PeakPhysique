e32377a157ad996b42ae23987192ed81025ff4e8
amo02008 <adrienne.osorio@gmail.com>
2021-04-01 15:10:13 -0400

    making changes so app can delpoy through heroku

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true,
       useFindAndModify: false
     }
   );

fc473b778e6836f58c9393bd71af3a4afab36eb4
amo02008 <adrienne.osorio@gmail.com>
2021-04-01 14:09:08 -0400

    updated mondodb connection

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 app.use(require("./routes/apiroutes.js"));
 app.use(require("./routes/html-routes.js"));
